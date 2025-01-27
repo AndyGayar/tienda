a36e4af27a075bb20f0315eea075e816ee5e1b7c
AndyGayar <andresgayarromero@gmail.com>
2025-01-27 16:53:36 +0100

    oi

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="username">Nombre de usuario</label>
+        <input type="text" name="username" id="username">
+        <label for="password">Contraseña</label>
+        <input type="text" name="password" id="password">
+
+    </form>
+</body>
+</html>
